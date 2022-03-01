a949491b4bab81efeaf3937cfa1df15affb6df6d
itsberriver <jmejiaos713@gmail.com>
2022-03-01 20:12:25 +0100

    chrono is useful

--- temporizador.js
+++ temporizador.js
@@ -2,12 +2,13 @@ let seconds = 0;
 let minutes = 0;
 let hours = 0;
 
+
 const startButton = document.getElementById('start');
 const pauseButton = document.getElementById('pause');
 const resumeButton = document.getElementById('resume');
 const resetButton = document.getElementById('reset');
 
-function startTimer(){
+function start(){
     
     seconds ++;
 
@@ -20,16 +21,34 @@ function startTimer(){
             hours++;
         }
     }
+
+    if (seconds < 10){
+        seconds = "0" + seconds.toString();
+    }
+    else{
+        seconds = seconds;
+    }
+
+    /*if (minutes < 10){
+        minutes = "0" + minutes.toString();
+    else{
+        minutes = minutes;
+    }
+    }*/
+
+
+
     document.getElementById("timer").innerHTML = hours + ":" + minutes + ":" + seconds;
 }
-
-setInterval(startTimer,1000)
-
+setInterval(start,1000)
 
 
 
 
-
+startButton.addEventListener ('click',start)
+pauseButton.addEventListener ('click',reset)
+resumeButton.addEventListener ('click',)
+resetButton.addEventListener ('click',)
 
 
 
