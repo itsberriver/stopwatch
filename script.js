cfc81ff73a9c5459a4742fcb83ec136d02ff8a7b
itsberriver <jmejiaos713@gmail.com>
2022-04-05 18:14:57 +0200

    solved start and reset buttons and changed timeout to interval

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let seconds = 0;
 let minutes = 0;
 let hours = 0;
+let interval;
 
 const startButton = document.getElementById('start');
 const stopButton = document.getElementById('stop');
@@ -17,54 +18,39 @@ function printNumbers(time, idName) {
 function startTimer() {
     if (seconds < 59) {
         seconds++;
-        // if (seconds < 10) {
-        //     seconds = "0" + seconds;
-        // }
+        
         printNumbers(seconds,'seconds');
     }
     if (seconds >= 59) {
         seconds = 0;
         minutes++;
-        // if (minutes < 10) {
-        //     minutes = "0" + minutes;
-        // }
+        
         printNumbers(minutes,'minutes');
     }
     if (minutes >= 59) {
         minutes = 0;
         hours++;
-        // if (hours < 10) {
-        //     hours = "0" + hours;
-        // }
+
         printNumbers(hours,'hours');
     }
-    // document.getElementById('seconds').innerHTML = seconds;
-    // document.getElementById('minutes').innerHTML = minutes;
-    // document.getElementById('hours').innerHTML = hours;
-
-    setTimeout(startTimer, 1000);
-
 }
 
-startButton.addEventListener('click', function () {
-    startTimer();
+startButton.addEventListener('click', function  () {
     startButton.disabled = true;
-    pauseButton.disabled = false;
+    // pauseButton.disabled = false;
     resetButton.disabled = false;
-
+    interval = setInterval(startTimer, 1000);
 });
 
 
 resetButton.addEventListener('click', function () {
+    clearInterval(interval);
     seconds = 0;
     minutes = 0;
     hours = 0;
     printNumbers(seconds,'seconds');
     printNumbers(minutes,'minutes');
     printNumbers(hours,'hours');
-    // document.getElementById('seconds').innerHTML = seconds;
-    // document.getElementById('minutes').innerHTML = minutes;
-    // document.getElementById('hours').innerHTML = hours;
     startButton.disabled = false;
     // pauseButton.disabled = true;
     resetButton.disabled = true;
