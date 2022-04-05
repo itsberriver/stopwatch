d29ab860a9d91084b3e48bf000e262e5d24741f8
itsberriver <jmejiaos713@gmail.com>
2022-04-05 17:47:40 +0200

    change code to add function to print correctly the double 00

--- script.js
+++ script.js
@@ -7,30 +7,40 @@ const stopButton = document.getElementById('stop');
 /*const resumeButton = document.getElementById('resume');*/
 const resetButton = document.getElementById('reset');
 
+function printNumbers(time, idName) {
+    if (time < 10) {
+        time = "0" + time;
+    }
+    document.getElementById(idName).innerHTML = time;
+}
+
 function startTimer() {
     if (seconds < 59) {
         seconds++;
-        if (seconds < 10) {
-            seconds = "0" + seconds;
-        }
+        // if (seconds < 10) {
+        //     seconds = "0" + seconds;
+        // }
+        printNumbers(seconds,'seconds');
     }
     if (seconds >= 59) {
         seconds = 0;
         minutes++;
-        if (minutes < 10) {
-            minutes = "0" + minutes;
-        }
+        // if (minutes < 10) {
+        //     minutes = "0" + minutes;
+        // }
+        printNumbers(minutes,'minutes');
     }
     if (minutes >= 59) {
         minutes = 0;
         hours++;
-        if (hours < 10) {
-            hours = "0" + hours;
-        }
+        // if (hours < 10) {
+        //     hours = "0" + hours;
+        // }
+        printNumbers(hours,'hours');
     }
-    document.getElementById('seconds').innerHTML = seconds;
-    document.getElementById('minutes').innerHTML = minutes;
-    document.getElementById('hours').innerHTML = hours;
+    // document.getElementById('seconds').innerHTML = seconds;
+    // document.getElementById('minutes').innerHTML = minutes;
+    // document.getElementById('hours').innerHTML = hours;
 
     setTimeout(startTimer, 1000);
 
@@ -49,14 +59,19 @@ resetButton.addEventListener('click', function () {
     seconds = 0;
     minutes = 0;
     hours = 0;
-    document.getElementById('timer').innerHTML = "00:00:00";
+    printNumbers(seconds,'seconds');
+    printNumbers(minutes,'minutes');
+    printNumbers(hours,'hours');
+    // document.getElementById('seconds').innerHTML = seconds;
+    // document.getElementById('minutes').innerHTML = minutes;
+    // document.getElementById('hours').innerHTML = hours;
     startButton.disabled = false;
     // pauseButton.disabled = true;
     resetButton.disabled = true;
-    stopTimer();
+    // stopTimer();
 });
 
-// stop.Button.addEventListener('click', function () {
+// stopButton.addEventListener('click', function () {
 //     stopTimer();
 //     startButton.disabled = false;
 
